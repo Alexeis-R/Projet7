090f29998eabba527baada60b73da15de82e57e1
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-24 12:52:57 +0200

    route correction

--- Kasa/src/main.jsx
+++ Kasa/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {RourterProvider} from "react-router-dom";
-import { Router } from "./Router";
+import {RouterProvider} from "react-router-dom";
+import  router  from "./Router";
 
 import "./index.scss";
 
