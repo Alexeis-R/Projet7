7156f6afcce5f4d87285bfa334ba625b6004c690
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-24 22:58:37 +0200

    Add Thumbs component

---
+++ Kasa/src/components/Thumbs/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Thumbs.scss';
+
+function Thumbs({cover, title}) {
+    return (
+        <article className="thumbnail">
+            <img className="thumbnail_img" src={cover} alt={title} />
+            <h2 className="thumbnail_title">{title}</h2>
+        </article>
+    );
+  }
+  
+  export default Thumbs;
