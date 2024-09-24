a8cb695e45a347ddbf8c8d9fc9c5df8d3c036992
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-24 12:55:28 +0200

    route correction

--- Kasa/src/Router/index.jsx
+++ Kasa/src/Router/index.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Logement } from "../pages/Logement";
-import { About }  from "../pages/About";
-import { Error } from "../pages/Error";
+import Home  from "../pages/Home";
+import Logement  from "../pages/Logement";
+import  About   from "../pages/About";
+import  Error from "../pages/Error";
 
 const router = createBrowserRouter([
     {
