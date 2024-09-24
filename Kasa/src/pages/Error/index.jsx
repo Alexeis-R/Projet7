382be09a6656bf36375877be11e56b888a5875ee
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-24 12:55:52 +0200

    add error page

--- Kasa/src/pages/Error/index.jsx
+++ Kasa/src/pages/Error/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './Error.css';
+import React from "react";
+import { Link } from 'react-router-dom'
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import "./Error.scss";
 
-const ErrorPage = () => {
-    return (
-        <div className="error-page">
-            <h1>404</h1>
-            <p>Oops! The page you are looking for does not exist.</p>
-        </div>
-    );
-};
+function ErrorPage() {
+  return (
+    <div>
+      <Header />
+      <div className="error_container">
+        <h1 className="error_container_title">404</h1>
+        <p className="error_container_text">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to ='/'>Retourner sur la page d’accueil</Link>
+      </div>
+      <Footer />
+    </div>
+  );
+}
 
 export default ErrorPage;
