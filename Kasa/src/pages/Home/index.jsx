6b9548dd5acad6f27c497ce2e0ec33e40e24c374
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-24 12:56:08 +0200

    add home page

--- Kasa/src/pages/Home/index.jsx
+++ Kasa/src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Banner from '../../components/Banner';
+import banner1 from '../../../public/banner.jpg';
+import Footer from '../../components/Footer';
+// import Thumbs from '../../components/Thumbs';
 
-const Home = () => {
+function Home() {
     return (
         <div>
-           <p>Page Home</p>
+          <Header />
+          <Banner 
+            bannerImg={banner1}
+            text="Chez vous, partout et ailleurs"
+          />
+          {/* <Thumbs /> */}
+          <Footer /> 
+         
         </div>
     );
 };
