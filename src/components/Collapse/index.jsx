17c3106dc1b1480a0346e830e4b8abb7c1cdd9d0
Alexeis-R <alexeisrocha1@icloud.com>
2024-09-26 12:53:36 +0200

    Update CSS formatting in Collapse component

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import "./Collapse.scss";
+
+function Collapse({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else {
+      contentRef.current.style.maxHeight = "0px";
+    }
+  }, [isOpen]);
+
+  function toggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className="collapse">
+      <button onClick={toggle}>
+        <span className="collapse-title">{title}</span>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className={`collapse-icon ${isOpen ? "open" : ""}`}
+        />
+      </button>
+      <div
+        ref={contentRef}
+        className={`collapse_content ${isOpen ? "open" : ""}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Collapse;
