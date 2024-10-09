52ee21302c696faf5f0418804a49417e312b5bd6
Alexeis-R <alexeisrocha1@icloud.com>
2024-10-09 10:27:17 +0200

    Update responsive styles for Banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import './Banner.scss';
+import React from "react";
+import "./Banner.scss";
 
 function Banner({ bannerImg, text }) {
   return (
     <div className="container">
       <div className="banner">
-      {text && <div className="banner_overlay"></div>}
-      <img
-        className="banner_img"
-        src={bannerImg}
-        alt="Banner"
-      />
-      {text && (
-        <p className="banner_text">{text}</p>
-      )}
-    </div>
+        <div className="banner_overlay"></div>
+        <img className="banner_img" src={bannerImg} alt="Banner" />
+        {text && <p className="banner_text">{text}</p>}
+      </div>
     </div>
   );
 }
