95a3ce776ef88c0c65ac18b2e93fb240a7607398
Alexeis-R <alexeisrocha1@icloud.com>
2024-10-09 10:30:24 +0200

    Update responsive styles for Error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
+import Layout from "../../components/Layout";
 import "./Error.scss";
 
 function ErrorPage() {
   return (
-    <div>
-      <Header />
+    <Layout>
       <div className="error_container">
         <h1 className="error_container_title">404</h1>
-        <p className="error_container_text">Oups! La page que vous demandez n'existe pas.</p>
-        <Link to ='/'>Retourner sur la page d’accueil</Link>
+        <p className="error_container_text">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link to="/">Retourner sur la page d’accueil</Link>
       </div>
-      <Footer />
-    </div>
+    </Layout>
   );
 }
 
