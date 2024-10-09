f6a98e67cfbe5b56084c2a74713f781520475c71
Alexeis-R <alexeisrocha1@icloud.com>
2024-10-09 10:31:15 +0200

    Update router configuration for error page

--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,27 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home  from "../pages/Home";
-import Logement  from "../pages/Logement";
-import  About   from "../pages/About";
-import  Error from "../pages/Error";
+import Home from "../pages/Home";
+import Logement from "../pages/Logement";
+import About from "../pages/About";
+import Error from "../pages/Error";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path:"/logement/:logementId",
-        element: <Logement />
-    },
-    {
-        path: "/about",
-        element: <About />
-
-    },
-    {
-        path: "*",
-        element: <Error />
-    },
-])
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/logement/:logementId",
+    element: <Logement />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 
 export default router;
